7de7e81deb94466a509037b8c3741399b0356579
Cara Lewis <3012459l@student.gla.ac.uk>
2026-03-03 17:32:10 +0000

    Add on delete to HaikuUser

--- five_seven_five_app/models.py
+++ five_seven_five_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class HaikuUser(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateField()
 
     def __str__(self):
