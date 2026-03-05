32489d49931cf0546ee755a90b4c58d0ae854761
Cara Lewis <3012459l@student.gla.ac.uk>
2026-03-05 16:42:52 +0000

    add models to admin page

--- five_seven_five_app/admin.py
+++ five_seven_five_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from five_seven_five_app.models import Haiku, HaikuUser, Profile, Comment, Like, Follow
 
 # Register your models here.
+admin.site.register(Haiku)
+admin.site.register(HaikuUser)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
